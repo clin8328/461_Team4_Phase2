d57c2877f5541e05df9b34b3bbc099cb56e16e9a
Junpei Ota <otaj@purdue.edu>
2023-12-13 23:31:14 -0500

    id to string for /post packages

--- src/utils/logMsg.ts
+++
@@ -1,7 +0,0 @@
-import { logMsg } from "./logger";
-
-// How To Run - logMsg(msg : string, option? : string)
-// msg - message being logged
-// option? - optionial type specifier - defaults to info
-
-logMsg("David : Begining Work On Github GraphQL API");
